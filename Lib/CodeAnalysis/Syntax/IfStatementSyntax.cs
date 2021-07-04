cd33e12b4c7c5ec78b1fc567e5d4a1e40c957892
jeremy <a283164069@outlook.com>
2021-07-04 13:28:50 +0800

    Add support for If statements

---
+++ Lib/CodeAnalysis/Syntax/IfStatementSyntax.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace complier.CodeAnalysis.Syntax
+{
+    public sealed class IfStatementSyntax : StatementSyntax
+    {
+        public SyntaxToken IfKeyword { get; }
+        public ExpressionSyntax Condition { get; }
+        public StatementSyntax ThenStatement { get; }
+        public ElseClauseSyntax ElseClause { get; }
+
+        public IfStatementSyntax(SyntaxToken ifKeyword, ExpressionSyntax condition, StatementSyntax thenStatement,
+            ElseClauseSyntax elseClause)
+        {
+            IfKeyword = ifKeyword;
+            Condition = condition;
+            ThenStatement = thenStatement;
+            ElseClause = elseClause;
+        }
+        public override SyntaxKind Kind => SyntaxKind.IfStatement;
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return IfKeyword;
+            yield return Condition;
+            yield return ThenStatement;
+            yield return ElseClause;
+
+        }
+    }
+}
