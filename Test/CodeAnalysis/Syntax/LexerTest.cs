2c7205f719ca9250598d885ac3c87b459d660f2c
jeremy <a283164069@outlook.com>
2021-07-08 22:35:46 +0800

    Swapped expected and actual parameter for Assert.Equals() in LexerTest.cs

--- Test/CodeAnalysis/Syntax/LexerTest.cs
+++ Test/CodeAnalysis/Syntax/LexerTest.cs
@@ -39,11 +39,10 @@ namespace Test.CodeAnalysis.Syntax
              
              Assert.Equal(2,tokens.Length);
 
-            Assert.Equal(tokens[0].Kind, t1Kind);
-            Assert.Equal(tokens[0].Text, t1Text);
-
-            Assert.Equal(tokens[1].Kind, t2Kind);
-            Assert.Equal(tokens[1].Text, t2Text);
+            Assert.Equal( t1Kind,tokens[0].Kind);
+            Assert.Equal( t1Text,tokens[0].Text);
+            Assert.Equal( t2Kind,tokens[1].Kind);
+            Assert.Equal( t2Text,tokens[1].Text);
       
         }
 
@@ -58,14 +57,14 @@ namespace Test.CodeAnalysis.Syntax
 
             Assert.Equal(3, tokens.Length);
 
-            Assert.Equal(tokens[0].Kind, t1Kind);
-            Assert.Equal(tokens[0].Text, t1Text);
+            Assert.Equal(t1Kind, tokens[0].Kind);
+            Assert.Equal(t1Text, tokens[0].Text);
 
-            Assert.Equal(tokens[1].Kind, separatorKind);
-            Assert.Equal(tokens[1].Text, separatorText);
+            Assert.Equal(separatorKind, tokens[1].Kind);
+            Assert.Equal(separatorText, tokens[1].Text);
 
-            Assert.Equal(tokens[2].Kind, t2Kind);
-            Assert.Equal(tokens[2].Text, t2Text);
+            Assert.Equal(t2Kind, tokens[2].Kind);
+            Assert.Equal(t2Text, tokens[2].Text);
 
         }
 
