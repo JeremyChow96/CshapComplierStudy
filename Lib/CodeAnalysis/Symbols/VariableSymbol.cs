84b1c97303a12a70e0820d61ad65b14e46d001b0
jeremy <a283164069@outlook.com>
2021-07-15 23:09:23 +0800

    Move VariableSymbol.cs to a separate file

---
+++ Lib/CodeAnalysis/Symbols/VariableSymbol.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lib.CodeAnalysis.Symbols
+{
+    public sealed class VariableSymbol :Symbol
+    {
+        public VariableSymbol(string name, bool isReadOnly, Type type) : base(name)
+        {
+         
+            IsReadOnly = isReadOnly;
+            Type = type;
+        }
+        
+        public bool IsReadOnly { get; }
+        public Type Type { get; }
+        public override string ToString() => Name;
+
+    }
+}
