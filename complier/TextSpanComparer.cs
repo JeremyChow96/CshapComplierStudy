7ef292b3802dfe3123baaef55786c3732e8b7435
jeremy <a283164069@outlook.com>
2021-08-15 11:07:24 +0800

    Sort diagnostics by span before showing them

---
+++ complier/TextSpanComparer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using complier.CodeAnalysis;
+
+internal class TextSpanComparer : IComparer<TextSpan>
+{
+    public int Compare(TextSpan x, TextSpan y)
+    {
+        int cmp = x.Start - y.Start;
+        if (cmp==0)
+        {
+            cmp = x.Length - y.Length;
+        }
+
+        return cmp;
+    }
+}
