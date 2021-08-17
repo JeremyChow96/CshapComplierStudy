5bfbe2d14045398e7a83f6047c70cda8298a4a1f
jeremy <a283164069@outlook.com>
2021-08-17 23:43:11 +0800

    Add support for break and continue

---
+++ Lib/CodeAnalysis/Binding/BoundLoopStatement.cs
@@ -0,0 +1,14 @@
+﻿namespace complier.CodeAnalysis.Binding
+{
+    internal abstract class LoopStatement : BoundStatement
+    {
+        public BoundLabel BreakLabel { get; }
+        public BoundLabel ContinueLabel { get; }
+
+        public LoopStatement(BoundLabel breakLabel, BoundLabel continueLabel)
+        {
+            BreakLabel = breakLabel;
+            ContinueLabel = continueLabel;
+        }
+    }
+}
